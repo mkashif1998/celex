e077f9b909c1793c9dfe9ab1aca81de73769b1d6
mkashif1998 <muhammadkashif3411@gmail.com>
2022-09-30 13:52:57 +0500

    upload

--- public/js/main.js
+++ public/js/main.js
@@ -2,7 +2,7 @@
     "use strict";
 
     /*----------------------------------------
-            Bootstrap dropdown               
+            Bootstrap dropdown
     -------------------------------------------*/
 
     // Add slideDown animation to Bootstrap dropdown when expanding.
@@ -39,7 +39,7 @@
     });
 
     /*---------------------------
-          Nice Select 
+          Nice Select
        ------------------------------ */
 
     $('select.shop-sort').niceSelect();
@@ -382,7 +382,7 @@
     });
 
     /*---------------------------
-        Quick view Slider 
+        Quick view Slider
     ------------------------------ */
     var galleryThumb = new Swiper('.gallery-thumbs', {
         spaceBetween: 10,
@@ -404,7 +404,7 @@
     });
 
     /*---------------------------
-        Product Details Slider 
+        Product Details Slider
     ------------------------------ */
     var zoomThumb = new Swiper('.zoom-thumbs', {
         spaceBetween: 10,
@@ -425,7 +425,7 @@
     });
 
     /*---------------------------
-        Product Details Slider 
+        Product Details Slider
     ------------------------------ */
     var zoomThumb = new Swiper('.zoom-thumbs-2', {
         spaceBetween: 0,
@@ -546,7 +546,7 @@
     });
 
     /*-----------------------------
-        Blog Gallery Slider 
+        Blog Gallery Slider
     -------------------------------- */
     var swiper = new Swiper(".blog-post-media", {
         slidesPerView: 1,
